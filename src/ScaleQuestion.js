8fc145da5eeee889dabed6beb4d1ece242b8a71e
dajinchu <dajinchu@gmail.com>
2018-12-26 22:04:46 -0500

    testing covering iframe

---
+++ src/ScaleQuestion.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { RadioGroup, Radio } from 'informed';
+import { scaleQuestionType } from './types';
+
+
+class ScaleQuestion extends React.Component {
+  renderChoices() {
+    const { id, choices } = this.props;
+    const radios = choices.reduce((acc, choice, idx) => {
+      const choiceId = `radio${id}-${idx}`;
+      // the key property is required by React to identify list elements.
+      acc.push(<Radio key={choiceId} value={choice} id={choiceId} />);
+      acc.push(<label key={`label${choiceId}`} htmlFor={choiceId}>{choice}</label>);
+      return acc;
+    }, []);
+    return radios;
+  }
+
+  render() {
+    const { id, label } = this.props;
+    return (
+      <div>
+        <label htmlFor={id}>{label}</label>
+        <RadioGroup id={id} field={id}>
+          {this.renderChoices()}
+        </RadioGroup>
+      </div>
+    );
+  }
+}
+
+ScaleQuestion.propTypes = scaleQuestionType;
+
+
+export default ScaleQuestion;
--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -8,6 +8,8 @@ class VideoPlayer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      lastPos: 0,
+      justReset: false,
       paused: false,
       data: [],
     };
@@ -21,6 +23,26 @@ class VideoPlayer extends React.Component {
     this.setState({ paused: false });
   }
 
+  // Keep track of video progress to override user skipping around.
+  // Would just be better to hide controls, but that requires Vimeo Plus
+  onProgress({ played }) {
+    this.setState({ lastPos: played });
+  }
+
+  // Reset the video if user tries skipping around.
+  onSeek() {
+    // Have to keep track of justReset to avoid looping onSeek.
+    const { justReset, lastPos } = this.state;
+    if (justReset) {
+      this.setState({ justReset: false });
+    } else {
+      this.setState({ justReset: true });
+      this.player.seekTo(lastPos);
+      // eslint-disable-next-line no-alert, no-undef
+      alert('Please do not skip around the video');
+    }
+  }
+
   // Add the new data point from VideoQuestions and resume the video
   onSubmit(newValue) {
     const { data } = this.state;
@@ -30,13 +52,25 @@ class VideoPlayer extends React.Component {
     });
   }
 
+  getPlayerRef(ref) {
+    this.player = ref;
+  }
+
   render() {
     const { videoId, questions } = this.props;
     const { paused } = this.state;
     const videoUrl = `https://vimeo.com/${videoId}`;
     return (
       <div>
-        <ReactPlayer url={videoUrl} onPause={() => this.onPause()} onPlay={() => this.onPlay()} />
+        <div id="opaqueLayer" />
+        <ReactPlayer
+          ref={r => this.getPlayerRef(r)}
+          url={videoUrl}
+          onPause={() => this.onPause()}
+          onPlay={() => this.onPlay()}
+          onProgress={p => this.onProgress(p)}
+          onSeek={() => this.onSeek()}
+        />
         {paused ? <VideoQuestions onSubmit={n => this.onSubmit(n)} questions={questions} /> : null}
       </div>
     );
