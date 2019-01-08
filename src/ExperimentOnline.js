f0e268ecfc969b17320ed76f408260c84bd5596e
dajinchu <dajinchu@gmail.com>
2019-01-08 18:09:26 -0500

    Close the experiment when video ends

--- src/ExperimentOnline.js
+++ src/ExperimentOnline.js
@@ -5,33 +5,51 @@ import Experiment from './Experiment';
 
 const SERVER_URL = 'http://142.93.49.129:3000';
 
-function sendData(collected) {
-  const time = Math.floor(new Date().getTime() / 1000);
-  const file = new File([JSON.stringify(collected)], `${time}.json`);
-
-  const formData = new FormData();
-  formData.append('file', file);
-
-  // Using axios http lib to send post request with formdata.
-  // fetch, xmlhttprequest, jquery etc. could also be used.
-  Axios.post(`${SERVER_URL}/data`, formData)
-    .then(response => console.log(response))
-    .catch(error => console.log(error));
-}
-
 /* In charge of talking to server */
-function ExperimentOnline() {
-  return (
-    <Fetch url={`${SERVER_URL}/experiment`} as="json">
-      {({ loading, error, data }) => (
-        <div>
-          {loading && <span>Loading...</span> }
-          {data && <Experiment {...data} sendData={(collected) => { sendData(collected); }} />}
-          {error && <span>Server error</span>}
-        </div>
-      )}
-    </Fetch>
-  );
+class ExperimentOnline extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false,
+    };
+  }
+
+  sendData(collected) {
+    const time = Math.floor(new Date().getTime() / 1000);
+    const file = new File([JSON.stringify(collected)], `${time}.json`);
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    // Using axios http lib to send post request with formdata.
+    // fetch, xmlhttprequest, jquery etc. could also be used.
+    Axios.post(`${SERVER_URL}/data`, formData)
+      .then((response) => {
+        console.log(response);
+        this.setState({ done: true });
+      })
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    const { done } = this.state;
+
+    if (done) {
+      return (<span>Thank you for participating. You can close this browser tab.</span>);
+    }
+
+    return (
+      <Fetch url={`${SERVER_URL}/experiment`} as="json">
+        {({ loading, error, data }) => (
+          <div>
+            {loading && <span>Loading...</span> }
+            {data && <Experiment {...data} sendData={(collected) => { this.sendData(collected); }} />}
+            {error && <span>Server error</span>}
+          </div>
+        )}
+      </Fetch>
+    );
+  }
 }
 
 export default ExperimentOnline;
