46123a4f7cf2bfad66df598c8cca76daaec8f5ca
TalUsvyatsky <usvyatsky.t@husky.neu.edu>
2019-02-10 14:56:40 -0500

    basic schema fields

---
+++ src/components/Configuration.js
@@ -0,0 +1,31 @@
+
+
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import Form from 'react-jsonschema-form';
+
+const schema = {
+  title: 'Todo',
+  type: 'object',
+  required: ['title'],
+  properties: {
+    title: { type: 'string', title: 'Title', default: 'A new task' },
+    done: { type: 'boolean', title: 'Done?', default: false },
+  },
+};
+
+const log = type => console.log.bind(console, type);
+
+function Configuration() {
+  return (
+    <Form
+      schema={schema}
+      onChange={log('changed')}
+      onSubmit={log('submitted')}
+      onError={log('errors')}
+  />
+  );
+}
+
+export default Configuration;
