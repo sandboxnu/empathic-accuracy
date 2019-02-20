82384fcbeccfaafe1561e9d65c2725de3f09f91d
dajinchu <dajinchu@gmail.com>
2019-02-20 00:45:11 -0500

    Run ExperimentOffline unless production

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import '../App.css';
 import ExperimentOnline from './ExperimentOnline';
+import ExperimentOffline from './ExperimentOffline';
 import AdminPanel from './AdminPanel';
+
+const PRODUCTION = process.env.NODE_ENV === 'production';
+const Experiment = PRODUCTION ? ExperimentOnline : ExperimentOffline;
 
 function AppRouter() {
   return (
     <Router>
       <div className="App">
         <Route path="/admin" exact component={AdminPanel} />
-        <Route path="/" exact component={ExperimentOnline} />
+        <Route path="/" exact component={Experiment} />
       </div>
     </Router>
   );
